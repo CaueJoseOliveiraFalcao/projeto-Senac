bda661833013086cc2eb634fbeda5965fc630bee
tenza <tenza@tenza.tenza>
2024-02-27 10:23:35 -0400

    header style

--- client/login/src/app/components/DashboardHeader.tsx
+++ client/login/src/app/components/DashboardHeader.tsx
@@ -1,28 +1,47 @@
 import Link from "next/link"
 import {FaSearch , FaBell} from 'react-icons/fa'
 import {TbMessageCircle2Filled} from 'react-icons/tb'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 export default function DashboardHeader() {
-    const [user , SetUser] = useState({username : '' , userimg : ''})
+    const [user , SetUser] = useState({userName : '' , userimg : ''})
+    const [showMenu , SetMenu] = useState(false)
+
+    useEffect(() => {
+        let value  = localStorage.getItem('rede-social:user')
+        if (value) {
+            SetUser(JSON.parse(value));
+        }
+    },[]);
     return (
-        <header >
-            <Link href='/'>Dashboard</Link>
-            <div>
-                <input type="text"   />
+        <header className="w-full bg-white flex  justify-between py-2 px-4 items-center shadow-md" >
+            <Link href='/' className="font-bold text-sky-900 text-lg">Dashboard</Link>
+            <div className="flex bg-zinc-100 items-center text-gray-600 px-3 py-1 rounded-full ">
+                <input type="text" className="bg-zinc-100 focus-visible:outline-none"  placeholder="Pesquisar"  />
                 <FaSearch/>
             </div>
-            <div>
-                <div>
-                    <button>
+            <div className=" flex gap-5 items-center text-gray-600 ">
+                <div className="flex gap-3 px-3">
+                    <button className="bg-zinc-200 p-2 rounded-full hover:bg-zinc-300">
                         <TbMessageCircle2Filled/>
                     </button>
-                    <button>
-                        <FaBell/>
+                    <button  className="bg-zinc-200 p-2 rounded-full hover:bg-zinc-300">
+                        <FaBell/>s
                     </button>
                 </div>
-                <div>
-                    <img src={user.userimg.length > 0 ? user.userimg : 'https://news.uie.com/static/img/avatar.png'} alt="Foto de perfil"/>
-                    <span>{user.username}</span>
+                <div className="relative">
+                    <button className="flex gap-2 items-center " onClick={(e) => (SetMenu(!showMenu))}>
+                        <img 
+                        src={user.userimg ? user.userimg : 'https://news.uie.com/static/img/avatar.png'} 
+                        alt="Foto de perfil"
+                        className="w-8 h-8 rounded-full"/>
+                        <span className="font-bold ">{user.userName}</span>
+                    </button>
+                    {showMenu && 
+                    <div className="absolute flex-col flex bg-white p-4 shadow-md rounded-md gap-2 border-t- whitespace-nowrap right-[-5px]">
+                        <Link href='' className="border-b">Editar Perfil</Link>
+                        <Link href=''>Logout</Link>
+                    </div>
+                    }
                 </div>
             </div>
         </header>
--- client/login/src/app/page.tsx
+++ client/login/src/app/page.tsx
@@ -1,10 +1,13 @@
+"use client"
+
 import Image from "next/image";
+import DashboardHeader from "./components/DashboardHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between bg-zinc-100">
+      <DashboardHeader/>
       <h1>Login Page</h1>
-      
     </main>
   );
 }
